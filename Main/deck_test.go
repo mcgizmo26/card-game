8aa146b0289091279ebd3f3ffda36c340020bf4d
mcgizmo26 <lonnie.mcgill@smartshyp.com>
2021-04-27 22:54:23 -0600

    Renaming newDeck_test.go to deck_test.go and adding a new test to the test file

--- Main/deck_test.go
+++ Main/deck_test.go
@@ -1,1 +1,37 @@
 package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
+
+	if len(d) != 40 {
+		t.Errorf("Expected deck length of 40, but got %v", len(d))
+	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card of Ace of Spades, but got %v", d[0])
+	}
+
+	if d[len(d)-1] != "Ten of Clubs" {
+		t.Errorf("Expected last card to be Ten of Clubs, but got %v", d[len(d)-1])
+	}
+}
+
+func TestSaveToDeckAndNewDeckToFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 40 {
+		t.Errorf("Expected deck length of 40, but got %v", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
+}
